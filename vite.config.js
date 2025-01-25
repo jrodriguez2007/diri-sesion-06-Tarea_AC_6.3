1a44c5ee29964eefcf5c61c5f5be006d47de0fcd
Juan Rodriguez <jrodriguez172007@hotmail.com>
2025-01-25 21:21:58 +0100

    Actualización para publicar en GitPages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "/diri-sesion-06-Tarea_AC_6.3/",
+    plugins: [react()]
+});
